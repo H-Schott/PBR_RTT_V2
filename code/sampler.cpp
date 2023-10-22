133827b687023e4d207bd417319ca0282107a5d0
Hugo Schott <hugo.schott@etu.univ-lyon1.fr>
2023-10-22 17:26:07 +0200

    created sampler files

---
+++ code/sampler.cpp
@@ -0,0 +1,12 @@
+#include "sampler.hpp"
+
+
+Sampler::Sampler() {
+
+}
+
+
+
+
+
+//EOF
---
+++ code/sampler.hpp
@@ -0,0 +1,16 @@
+#ifndef __SAMPLER_HPP__
+#define __SAMPLER_HPP__
+
+#include <vector>
+
+#include "point.hpp"
+#include "vector.hpp"
+
+class Sampler {
+public:
+
+	Sampler();
+
+};
+
+#endif
