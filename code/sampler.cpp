91f877116373967759adf379b3b44473a67aa107
Hugo Schott <hugo.schott@etu.univ-lyon1.fr>
2023-10-22 22:16:41 +0200

    created sampler classes

--- code/sampler.cpp
+++ code/sampler.cpp
@@ -1,10 +1,47 @@
 #include "sampler.hpp"
 
 
-Sampler::Sampler() {
+Sampler::Sampler(unsigned int s) : seed(s) {
+	n = 0;
+}
+
+
+unsigned int Sampler::RandInt() {
+	unsigned int random = 0;
+
+	// TODO
+
+	n++;
+	return random;
+}
+
+
+// UNIFORM SAMPLER ----------------------------
+
+UniformSampler::UniformSampler(unsigned int s) : Sampler(s) {
 
 }
 
+double UniformSampler::operator()() {
+	unsigned int randint = RandInt();
+	double random = double(randint) / max_rand_int;
+
+	return random;
+}
+
+
+// GAUSSIAN SAMPLER ----------------------------
+
+GaussianSampler::GaussianSampler(unsigned int s) : Sampler(s) {
+	u_sampler = UniformSampler(seed);
+}
+
+
+double GaussianSampler::operator()() {	
+	double random = u_sampler();
+
+	return random;
+}
 
 
 
--- code/sampler.hpp
+++ code/sampler.hpp
@@ -9,8 +9,36 @@
 class Sampler {
 public:
 
-	Sampler();
+	unsigned int seed = 0;
+	unsigned int n = 0;
+	unsigned int max_rand_int = UINT_MAX;
 
+	Sampler(unsigned int s = 0);
+
+	unsigned int RandInt();
+
+	virtual double operator()() = 0;
+
+};
+
+
+class UniformSampler : public Sampler {
+public:
+
+	UniformSampler(unsigned int s = 0);
+
+	virtual double operator()() override;
+};
+
+
+class GaussianSampler : public Sampler {
+public:
+
+	UniformSampler u_sampler;
+
+	GaussianSampler(unsigned int s = 0);
+
+	virtual double operator()() override;
 };
 
 #endif
